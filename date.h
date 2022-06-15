479780fca2eb12b09e5e8b87211af60703583e33
Ariadna Vigo <ariadna@ariadnavigo.xyz>
2022-06-15 16:21:22 +0200

    date.h: fixed gcc warn about buffer size

--- date.h
+++ date.h
@@ -1,6 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
-#define DATE_SIZE 11 /* "YYYY-MM-DD\0" = 11 chars */
+/* 
+ * "YYYY-MM-DD\0" is 11 chars, but gcc wants >36. 42 is the next multiple of 
+ * 8, so using that. 
+ */
+#define DATE_SIZE 42
 
 const char *date_str(time_t date);
 int is_date(const char *str);
